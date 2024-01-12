6ce24bf42490852f784279c993f66de6197f204d
Hitesh Saini <hitesh9694saini@gmail.com>
2024-01-12 20:00:08 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://videoplayerapi.onrender.com',
+    },
+  },
+
+  routes: [
+    { path: '/', component: '/src/components/Layout.jsx' },
+    { path: '/AddVideo', component: '/src/components/AddVideo.jsx' },
+    { path: '/seevideos/:id/:subtitle_id', component: '/src/components/SeeVideo.jsx' },
+  ],
 })
